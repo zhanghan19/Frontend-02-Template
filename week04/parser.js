8200ecf06bb3dd9787b213d1e1c1a549b2c74790
zhanghan <zhanhan321@126.com>
2020-07-23 16:59:31 +0800

    week04-company02

--- week04/parser.js
+++ week04/parser.js
@@ -1,7 +1,55 @@
 let currentToken = null;
+let currentAttribute = null;
+
+let stack = [{type: "document", children: []}];
 
 function emit(token) {
-
+	
+	let top = stack[stack.length - 1];
+	
+	if(token.type == "startTag") {
+		let element = {
+			type: "element",
+			children: [],
+			attributes: []
+		};
+		
+		element.tagName = token.tagName;
+		
+		for(let p in token) {
+			if(p != "type" && p != "tagName") {
+				element.attributes.push({
+					name: p,
+					value: token[p]
+				});
+			}
+		}
+		
+		top.children.push(element);
+		element.parent = top;
+		
+		if(!token.isSelfClosing){
+			stack.push(element);
+		}
+		
+		currentTextNode = null;
+	} else if(token.type == "endTag") {
+		if(top.tagName != token.tagName) {
+			throw new Error("Tag start end doesn't match!");
+		} else {
+			stack.pop();
+		}
+		currentTextNode = null;
+	} else if(token.type === "text"){
+		if(currentTextNode == null) {
+			currentTextNode = {
+				type: "text",
+				content: ""
+			}
+			top.children.push(currentTextNode);
+		}
+		currentTextNode.content += token.content;
+	}
 }
 
 const EOF = Symbol("EOF");
